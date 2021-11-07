77833d4be6a03c5a2e3ede86248ccbb18bba4b3d
Utkarsh Gangwar <utkarshgangwar366@gmail.com>
2021-11-07 21:17:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-let a = document.getElementById("sum");
-a.addEventListener("click",find());
-function find(){
-    console.log("sdf");
+let text="";
+let num1=0;
+let num2=0;
+let op=" ";
+let display = document.getElementById("disp-box");
+function data(num){
+    if(op == " "){
+        num1 = num1*10+num;
+    }
+    else
+    num2 = num2*10+num;
+    text_update();
+}
+
+function text_update(){
+    // console.log(num1+op+"/");
+    if(op == " "){
+        console.log(num1);
+        text = num1;
+    }
+    console.log(text);
+    display.innerHTML=text;
 }
